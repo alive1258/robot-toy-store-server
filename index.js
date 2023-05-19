f1505927b50c25e23a40562eb0614ad123748f2c
alive1258 <javashazib@gmail.com>
2023-05-19 16:34:43 +0600

    single toy data load code create done

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
-const express=require('express')
-const app = express()
-const cors =require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
-const port =process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
-
-console.log(process.env.DB_PASS)
-
-
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1yov7jc.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -22,16 +17,36 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const productCollection = client.db("robotToy").collection("products");
+
+    // all toy data load
+    app.get("/products", async (req, res) => {
+      const cursor = productCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // single toy data load
+    app.get("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await productCollection.findOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -39,12 +54,10 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("robot toy store is Booking");
+});
 
-
-app.get('/',(req,res)=>{
-    res.send("robot toy store is Booking");
-})
-
-app.listen(port,()=>{
-    console.log(`robot toy store is running port: ${port}`)
-})
+app.listen(port, () => {
+  console.log(`robot toy store is running port: ${port}`);
+});
